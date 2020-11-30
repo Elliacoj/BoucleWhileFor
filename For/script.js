69ddfc1f24b1ee16f555e9e25653208bc4dc1394
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2020-11-30 15:03:58 +0100

    Modif

---
+++ For/script.js
@@ -0,0 +1,3 @@
+for (let boucle = 0; boucle < 11; boucle++) {
+    console.log (boucle)
+}
---
+++ While/script.js
@@ -0,0 +1,5 @@
+let boucle = 0;
+while (boucle < 11) {
+    console.log ("La boucle vaut actuellement " + boucle);
+    boucle++;
+}
